31269b1e07371dcf9498beb3b4ad41e079be3ba8
Leen Blom <pi@lblom.nl>
2024-07-29 09:00:22 +0200

    Moved the globals to Flask aware context processor

--- ragservice.py
+++ ragservice.py
@@ -1,28 +1,33 @@
-from urllib.request import urlopen
-from urllib.parse import quote
-from flask import Flask, make_response, request
-from flask_cors import CORS, cross_origin
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_chroma import Chroma
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.messages import BaseMessage, AIMessage
-from langchain.schema.messages import HumanMessage, AIMessage
-from langchain_community.document_loaders import DirectoryLoader,TextLoader
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter, Language
-from langchain_core.pydantic_v1 import BaseModel, Field
+"""_summary_
+RAG for your own documents
+"""
 import logging
-import base64
 import os
 import sys
 import uuid
 import importlib
-import config
+import base64
 from typing import List
+from urllib.request import urlopen
+from urllib.parse import quote
+from flask import Flask, make_response, request
+from werkzeug.exceptions import HTTPException
+from flask_cors import CORS, cross_origin
+from langchain.schema.messages import HumanMessage, AIMessage
+from langchain.chains import create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import DirectoryLoader,TextLoader
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.messages import BaseMessage
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter, Language
+
+import config
 
 print(f"Arguments count: {len(sys.argv)}")
 for i, arg in enumerate(sys.argv):
@@ -32,18 +37,25 @@ constants = importlib.import_module("constants.constants_"+PROJECT)
 app = Flask(__name__)
 CORS(app)
 
+@app.context_processor
+def context_processor():
+    """ Store the globals in a Fals way """
+    return dict()
+
 # Configureer logging
 logging.basicConfig(level=logging.getLevelName(constants.LOGGING_LEVEL))
 
 os.environ["OPENAI_API_KEY"] = config.APIKEY
 
-modelText = "gpt-4o"
-temperatureText = constants.TEMPERATURE
-llm = ChatOpenAI(model=modelText, temperature=float(temperatureText))
-index = None
-chain = None
-store = {}
-session = uuid.uuid4()
+globvars = context_processor()
+globvars['ModelText']   = "gpt-4o"
+globvars['Temperature'] = float(constants.TEMPERATURE)
+globvars['Chain']       = None
+globvars['Store']       = {}
+globvars['Session']     = uuid.uuid4()
+globvars['LLM']         = ChatOpenAI(model=globvars['ModelText'], 
+                                     temperature=globvars['Temperature'])
+
 
 class InMemoryHistory(BaseChatMessageHistory, BaseModel):
     """In memory implementation of chat message history."""
@@ -58,28 +70,37 @@ class InMemoryHistory(BaseChatMessageHistory, BaseModel):
         self.messages = []
 
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
-    global store
+    """ Memory history store
+
+    Args:
+        session_id (str): no of the session
+
+    Returns:
+        BaseChatMessageHistory: the reference to the store
+    """    
     prefix = ""
-    if session_id not in store:
-        store[session_id] = InMemoryHistory()
-    for message in store[session_id].messages:
+    Store = globvars['Store']
+    if session_id not in Store:
+        Store[session_id] = InMemoryHistory()
+    for message in Store[session_id].messages:
         if isinstance(message, AIMessage):
             prefix = "AI"
         else:
             prefix = "User"
-        print(f"{prefix}: {message.content}\n")
-    return store[session_id]
+        print("%s: %s\n",prefix, message.content)
+    return Store[session_id]
 
 
-def encode_image(image_url):
-    logging.info(f"Encoding image: {image_url}")
+def encode_image(image_url) -> base64:
+    """ Encode image to base64 """
+    logging.info("Encoding image: %s",image_url)
     with urlopen(image_url) as url:
         f = url.read()
         image = base64.b64encode(f).decode("utf-8")
     return image
 
-def initialize_chain(model=llm):
-    global index, chain, store, session
+def initialize_chain(thisModel=globvars['LLM']):
+    """ initialize the chain to access the LLM """
 
     text_loader_kwargs={'autodetect_encoding': True}
 
@@ -95,8 +116,7 @@ def initialize_chain(model=llm):
     vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
     retriever = vectorstore.as_retriever(search_kwargs={"k": 8})
 
-
-### Contextualize question ###
+    ### Contextualize question ###
     contextualize_q_system_prompt = constants.contextualize_q_system_prompt
     
     contextualize_q_prompt = ChatPromptTemplate.from_messages(
@@ -107,10 +127,10 @@ def initialize_chain(model=llm):
         ]
     )
     history_aware_retriever = create_history_aware_retriever(
-        model, retriever, contextualize_q_prompt
+        thisModel, retriever, contextualize_q_prompt
     )
 
-### Answer question ###
+    ### Answer question ###
     system_prompt = constants.system_prompt + (
         "{context}"
     )
@@ -121,12 +141,12 @@ def initialize_chain(model=llm):
             ("human", "{input}"),
         ]
     )
-    question_answer_chain = create_stuff_documents_chain(model, qa_prompt)
+    question_answer_chain = create_stuff_documents_chain(thisModel, qa_prompt)
 
     rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
-    logging.info(f"Chain initialized: {modelText}")
-    chain = RunnableWithMessageHistory(
+    logging.info("Chain initialized: %s",globvars['ModelText'])
+    globvars['Chain'] = RunnableWithMessageHistory(
         rag_chain,
         get_session_history=get_session_history,
         input_messages_key="input",
@@ -139,48 +159,54 @@ def initialize_chain(model=llm):
 @app.route("/prompt/"+constants.ID, methods=["GET", "POST"])
 @cross_origin()
 def process():
-    global store, chain
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """   
     try:
         query = request.values['prompt']
-        logging.info(f"Query: {query}")
-        result = chain.invoke(
+        logging.info("Query: %s", query)
+        result = globvars['Chain'].invoke(
             {"input": query},
-            config={"configurable": {"session_id": session}},
+            config={"configurable": {"session_id": globvars['Session']}},
         )
-        logging.info(f"Result: {result['answer']}")
+        logging.info("Result: %s",result['answer'])
         return make_response(result['answer'], 200)
-    except Exception as e:
-        logging.error(f"Error processing prompt: {str(e)}")
+    except HTTPException as e:
+        logging.error("Error processing prompt: %s",str(e))
         return make_response("Error processing prompt", 500)
 
 @app.route("/prompt/model", methods=["GET", "POST"])
 @app.route("/prompt/"+constants.ID+"/model", methods=["GET", "POST"])
 @cross_origin()
-def model():
-    global chain, modelText, llm, temperatureText
+def model() -> make_response:
+    """ Change LLM model """
     try:
-        modelText = request.values['model']
-        llm = ChatOpenAI(model=modelText, temperature=float(temperatureText))
-        initialize_chain(llm)
-        logging.info(f"Model set to: {modelText}")
-        return make_response("Model set to: {modelText}" , 200)
-    except Exception as e:
-        logging.error(f"Error setting model: {str(e)}")
+        globvars['ModelText'] = request.values['model']
+        globvars['LLM'] = ChatOpenAI(model=globvars['ModelText'], 
+                                     temperature=globvars['Temperature'])
+        initialize_chain(globvars['LLM'])
+        logging.info("Model set to: %s", globvars['ModelText'])
+        return make_response("Model set to: " + globvars['ModelText'] , 200)
+    except HTTPException as e:
+        logging.error("Error setting model: %s", str(e))
         return make_response("Error setting model", 500)
 
 @app.route("/prompt/temp", methods=["GET", "POST"])
 @app.route("/prompt/"+constants.ID+"/temp", methods=["GET", "POST"])
 @cross_origin()
-def temp():
-    global chain, llm, temperatureText
+def temp() -> make_response:
+    """ Change LLM temperature """
     try:
-        temperatureText = request.values['temp']
-        llm = ChatOpenAI(model=modelText, temperature=float(temperatureText))
-        initialize_chain(llm)
-        logging.info(f"Temperature set to {temperatureText}")
-        return make_response("Temperature set to: "+temperatureText , 200)
-    except Exception as e:
-        logging.error(f"Error setting temperature {str(e)}")
+        globvars['Temperature'] = float(request.values['temp'])
+        globvars['LLM'] = ChatOpenAI(model=globvars['ModelText'], 
+                                     temperature=globvars['Temperature'])
+        initialize_chain(globvars['LLM'])
+        logging.info("Temperature set to %s", str(globvars['Temperature']))
+        return make_response("Temperature set to: " + str(globvars['Temperature']) , 200)
+    except HTTPException as e:
+        logging.error("Error setting temperature %s", str(e))
         return make_response("Error setting temperature", 500)
 
 
@@ -188,35 +214,36 @@ def temp():
 @app.route("/prompt/"+constants.ID+"/reload", methods=["GET", "POST"])
 @cross_origin()
 def reload():
-    global chain
+    """ Reload documents to the chain """
     try:
         initialize_chain()
         return make_response("Text reloaded", 200)
-    except Exception as e:
-        logging.error(f"Error reloading text: {str(e)}")
+    except HTTPException as e:
+        logging.error("Error reloading text: %s" ,str(e))
         return make_response("Error reloading text", 500)
+
 
 @app.route("/prompt/clear", methods=["GET", "POST"])
 @app.route("/prompt/"+constants.ID+"/clear", methods=["GET", "POST"])
 @cross_origin()
-def clear():
-    global chain,store
-    store.clear()
+def clear() -> make_response:
+    """ Clear the cache """
+    globvars['Store'].clear()
     return make_response("History deleted", 200)
 
 @app.route("/prompt/cache", methods=["GET", "POST"])
 @app.route("/prompt/"+constants.ID+"/cache", methods=["GET", "POST"])
 @cross_origin()
 def cache():
-    global store, session
+    """ Return cache contents """
     content = ""
-    if session  in store:
-        for message in store[session].messages:
+    if globvars['Session'] in globvars['Store']:
+        for message in globvars['Store'][globvars['Session']].messages:
             if isinstance(message, AIMessage):
                 prefix = "AI"
             else:
                 prefix = "User"
-            content += f"{prefix}: {message.content}\n"
+            content += prefix +  ":" + str(message) + "\n"
     return make_response(content, 200)
 
 
@@ -224,12 +251,14 @@ def cache():
 @app.route("/prompt/"+constants.ID+"/image", methods=["GET", "POST"])
 @cross_origin()
 def process_image():
+    """ Send image to ChatGPT and send prompt to analyse contents """
     try:
         image_url = quote(request.values['image'], safe='/:?=&')
         text = request.values['prompt']
-        logging.info(f"Processing image: {image_url}, with prompt: {text}")
+        logging.info("Processing image: %s, with prompt: %s", image_url, text)
         bimage = encode_image(image_url)
-        chain = ChatOpenAI(model=modelText, temperature=float(temperatureText))
+        chain = ChatOpenAI(model=globvars['ModelText'], 
+                           temperature=globvars['Temperature'])
         msg = chain.invoke(
             [
                 AIMessage(content="Gerco's foto ontdekker"),
@@ -240,10 +269,9 @@ def process_image():
             ]
         )
         return make_response(msg.content, 200)
-    except Exception as e:
-        logging.error(f"Error processing image: {str(e)}")
+    except HTTPException as e:
+        logging.error("Error processing image: %s", str(e))
         return make_response("Error processing image", 500)
-
 
 if __name__ == '__main__':
     initialize_chain()
